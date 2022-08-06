d164adb68528425d9ab9e6a5d3810efefaba6f64
Matt Woelfel <mwoelfel13@gmail.com>
2022-08-06 13:02:46 -0500

    adding basic chat functionality

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,15 +1,49 @@
 import express from "express";
 import * as trpc from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors";
+import { z } from "zod";
+
+interface ChatMessage {
+  user: string;
+  message: string;
+}
+
+const messages: ChatMessage[] = [
+  { user: "matt", message: "hello" },
+  { user: "missy", message: "goodbye" },
+];
 
 // How you manage routes (query, mutation, subscription routes)
-const appRouter = trpc.router().query("hello", {
-  resolve() {
-    return "sup";
-  },
-});
+// can split these into multiple files and combine using appRouter.merge
+const appRouter = trpc
+  .router()
+  .query("hello", {
+    resolve() {
+      return "hello world";
+    },
+  })
+  .query("getMessages", {
+    input: z.number().default(10),
+    resolve({ input }) {
+      return messages.slice(-input);
+    },
+  })
+  .mutation("addMessage", {
+    input: z.object({
+      user: z.string(),
+      message: z.string(),
+    }),
+    resolve({ input }) {
+      messages.push(input);
+      return input;
+    },
+  });
+
+export type AppRouter = typeof appRouter;
 
 const app = express();
+app.use(cors());
 const port = 8080;
 
 app.use(
